eae95d8755136150e0aa42a2f8b509b4a0908fcb
joparino <nikstrozzz@gmail.com>
2021-12-04 11:56:06 +0700

    request2.1

--- lib/request/request.dart
+++ lib/request/request.dart
@@ -1,11 +1,13 @@
-import 'dart:async';
+import 'dart:async' show Future, TimeoutException;
+import 'package:flutter/services.dart' show rootBundle;
 import 'dart:collection';
 import 'dart:convert';
+import 'package:flutter/animation.dart';
 import 'package:http/http.dart' as http;
 
 main() async {
   Uri url = Uri.parse('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1');
-   Map<String, String> headers = HashMap();
+  Map<String, String> headers = HashMap();
   headers.addAll({'X-API-KEY': 'c02232f2-6940-45e2-be79-bc333cac1da7'});
   headers.addAll({'content-type': 'application/json'});
 
@@ -14,9 +16,11 @@ main() async {
       headers: headers,
   );
   
-  print('Response status: ${response.statusCode}');
-  print('Response body: ${(response.body)}');
-  print('Response body: ${response.headers}');
+  var responseText = utf8.decode(response.bodyBytes);
+  Map<String, dynamic> x = json.decode(responseText);
+  print(x["pagesCount"]);
+
+  
 
   try {
     http.Response response = await http.get(
