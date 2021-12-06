ca111fd31cf16734dcbfad2fa251516198f7ec8a
joparino <nikstrozzz@gmail.com>
2021-12-06 19:07:07 +0700

    refactoring

--- lib/pages/profile/grid_view.dart
+++ lib/pages/profile/grid_view.dart
@@ -1,25 +1,27 @@
+import 'dart:typed_data';
+
 import 'package:flok/components/constants.dart';
 import 'package:flutter/material.dart';
 
-class List extends StatelessWidget {
-  const List({ Key? key, required this.image}) : super(key: key);
+class GridWidget extends StatelessWidget {
+  const GridWidget({ Key? key, required this.image}) : super(key: key);
 
   final String image;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
         GestureDetector(
           onTap: () {},
           child: Container(
-            margin: EdgeInsets.only(top: 10, left: 14),
+            margin: EdgeInsets.only(bottom: 2),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(25.0),
               child: Image.asset(
                 image,
-                width: cwidth,
-                height: cheight,
+                width: cwidth-20,
+                height: cheight-40,
                 fit: BoxFit.fill,
               ),
             ),
